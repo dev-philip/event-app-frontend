91b9384ff9d0b0b169be56cc55a3df86a3e10562
Philip Awobusuyi <talkwithdevphilip@gmail.com>
2024-02-12 14:55:29 -0500

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { environment } from '../environments/environment';
+import { LoginPageComponent } from "./pages/login-page/login-page.component";
 
 
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, RouterOutlet, LoginPageComponent]
 })
 export class AppComponent {
   title = 'event-app-frontend';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from '../app/pages/login-page/login-page.component';
+import { SignupPageComponent } from '../app/pages/signup-page/signup-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginPageComponent },
+    { path: 'login', component: LoginPageComponent },
+    { path: 'signup', component: SignupPageComponent },
+    { path: '**', redirectTo: '' } // Redirect to home if no matching route found
+  ];
